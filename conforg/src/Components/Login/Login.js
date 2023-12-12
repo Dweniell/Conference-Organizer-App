df828d7545d269e56de50e955a09e6279a440f0e
ienasoaie-nectarie-1066 <ienasoaienectarie21@stud.ase.ro>
2023-12-12 14:54:08 +0200

    forgot the code, oops

--- conforg/src/Components/Login/Login.js
+++ conforg/src/Components/Login/Login.js
@@ -1,6 +1,7 @@
 import './Login.css';
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios'
 function Login(){
     const navig = useNavigate()
     const handleClickBack = () =>{
@@ -9,14 +10,26 @@ function Login(){
     const handleContinue = () =>{
         navig('/main')
     }
+    const onFinish = values =>{
+        const {username, password} = values
+        axios.post('https://localhost:3001/validatePassword', {username,password})
+          .then(res=>{
+            if(res.data.validation){
+              alert('Password is correct')
+            }
+            else{
+              alert('Your password is not correct')
+            }
+          })
+    }
     return(
         <div className='Login'>
           <header className='Login-header'>
-            <input type='text' placeholder='username' className='inputs'></input>
-            <input type='text' placeholder='password' className='inputs'></input>
+            <input type='text' placeholder='username' className='inputs' id='username'></input>
+            <input type='text' placeholder='password' className='inputs' id='password'></input>
             <div className='lButtons'>
               <button type='button' className='lButtonStyle b2'onClick={handleClickBack}>Cancel</button>
-              <button type='button' className='lButtonStyle b1'onClick={handleContinue}>Continue</button>
+              <button type='button' className='lButtonStyle b1'onClick={onFinish}>Continue</button>
             </div>
           </header>
         </div>
